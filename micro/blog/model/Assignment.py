2e26d0c7716244e685afd2bdc8996e6d190a85d0
suyf <suyf@smartbow.net>
2017-11-06 16:57:15 +0800

    Signed-off-by: suyf <suyf@smartbow.net>

--- micro/blog/model/Assignment.py
+++ micro/blog/model/Assignment.py
@@ -4,19 +4,47 @@ Created on 2016年6月10日
 
 @author: Su
 '''
-from mongo.BaseMongodb import BaseMongodb
-import uuid
-class AssignmentDao(BaseMongodb):
-    def __init__(self, session=None):
-        BaseMongodb.__init__(self)
-        pass
-    def create_local_auth(self,password,email,name):
-        user_id = str("weizuoye_")+str(uuid.uuid1())
-        self.db.local_user.insert({"password":password,"email":email,"name":name,"user_id":user_id})
-        return user_id
-    def find_by_name(self,name):
-        user = self.db.local_user.find_one({"name":name})
-        return user
-    def find_by_email(self,email):
-        user = self.db.local_user.find_one({"email":email})
-        return user
+import datetime
+from mongoengine import *
+
+from BaseMongodb import connect
+# connect('mydb')
+
+class BaseMo(Document):
+    update_time = DateTimeField(default=datetime.datetime.utcnow)
+    meta = {'allow_inheritance': True}
+class ClassGroup(Document):
+    name = StringField(max_length=30)
+    num = StringField(min_value=5, max_value=20)
+    users = ListField(StringField(max_length=100))
+
+class Student(Document):
+    name = StringField(max_length=30)
+    email = EmailField(required=False)
+    password = StringField(max_length=50)
+    classgroup = ReferenceField(ClassGroup, reverse_delete_rule=PULL)
+class Teacher(Document):
+    name = StringField(max_length=30)
+    email = EmailField(required=False)
+    password = StringField(max_length=50)
+    meta = {'db_alias': 'db-teacher'}
+
+class Subject(Document):
+    name = StringField(max_length=20,required=False)
+    creat_time = DateTimeField(default=datetime.datetime.utcnow)
+    identifier = StringField(max_length=10,required=False)
+
+if __name__ == '__main__':
+    cg = ClassGroup()
+    cg.num = '001'
+    cg.name = '软件工程系'
+    cg.save()
+
+    st = Student()
+    st.name='suyf'
+    st.classgroup = cg
+    st.save()
+    # st = Student.objects(name='suyf')
+    # st.classgroup = cg
+
+    # import pdb;pdb.set_trace()
--- micro/blog/model/BaseMongodb.py
+++ micro/blog/model/BaseMongodb.py
@@ -4,12 +4,10 @@ Created on 2016年6月11日
 
 @author: Su
 '''
-import settings
-import pymongo
-class BaseMongodb():
-    def __init__(self):
-        print settings.options.mongo
-        conn  = pymongo.MongoClient(
-            host=settings.options.mongo
-            )
-        self.db = conn.workonline
+from mongoengine import *
+
+connect('weizuoye', host='127.0.0.1', port=27017)
+
+connect('teacher', alias='db-teacher',host='127.0.0.1', port=27017)
+
+connect('student', host='127.0.0.1', port=27017)
