b3b0857de869fcb4dc9b1ffc4d988afec61174fc
root <root@localhost.localdomain>
2017-11-13 10:06:46 -0500

    fix bug & add collegeclass user course

--- micro/blog/controller/__init__.py
+++
@@ -1,5 +0,0 @@
-from reg import *
-from blog import *
-from login import *
-from course import *
-from task import *
--- micro/blog/library/rbac/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-"""Simple RBAC
-
-This is a simple role based access control utility in Python.
-"""
-
-__all__ = ["acl", "context", "proxy"]
